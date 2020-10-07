e54cf5bced071e9d980311eafc3609e51ec2c84f
pathilink <pathilink@gmail.com>
2020-10-07 04:18:28 -0300

    upload exercise 8 to 10

---
+++ ex8.py
@@ -0,0 +1,11 @@
+formatter = "{} {} {} {}"
+# curly brackets accept everything
+print(formatter.format(1, 2, 3, 4)) # format is a function
+print(formatter.format("one", "two", "three", "four"))
+print(formatter.format(True, False, False, True))
+print(formatter.format(formatter, formatter, formatter, formatter))
+print(formatter.format(
+    "Try your",
+    "Own text here",
+    "Maybe a poem",
+    "Or a song about fear"))
