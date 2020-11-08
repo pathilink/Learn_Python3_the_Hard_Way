ce0464fbaba8b2868d3a1b8bcfeca166ef0b1a78
pathilink <pathilink@gmail.com>
2020-11-07 23:45:05 -0300

    add ex 31 to 34

---
+++ ex34.py
@@ -0,0 +1,12 @@
+animals = ['bear', 'python3.6', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+print(f'1. The animal at 1: {animals[1]}')
+print(f'2. The third (3rd) animal: {animals[2]}')
+print(f'3. The first (1st) animal: {animals[0]}')
+print(f'4. The animal at 3: {animals[3]}')
+print(f'5. The fifth (5th) animal: {animals[4]}')
+print(f'6. The animal at 2: {animals[2]}')
+print(f'7. The sixth (6th) animal: {animals[5]}')
+print(f'8. The animal at 4: {animals[4]}')
+
+# to run: $ python3 ex34.py
